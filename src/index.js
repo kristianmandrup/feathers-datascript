110dcec99f79dec1e1984250bf6cd8d13060a79a
Kristian Mandrup <kmandrup@gmail.com>
2016-03-20 00:27:51 +0000

    starting query-builder

--- src/index.js
+++ src/index.js
@@ -38,11 +38,43 @@ export const Service = Proto.extend({
     });
   },
 
+  /*
+    Pagination:
+      $limit: max # or records to return 
+        {$limit: 10}
+      $skip: how many records to skip before starting query 
+        {$skip: 100}
+
+    Sorting:
+      $sort: {name: 1} // name:ascending
+      $sort: {age: -1} // age:descending
+
+    Select:
+      $select: {'name':1} // only name
+      $select: {'password':0} // all except password
+
+    Where
+      ==
+      {name: 'Alice'} // all where name == 'Alice'
+
+      $in, $nin: Contains (or not) given value in list
+        {name: {$in: ['Alice', 'Bob']}}
+      $lt, $lte: less than <, <=
+        {age: {$lte: 65 }}
+
+      $gt, $gte: less than >, >=
+        {age: {$gte: 21 }}
+
+      $ne: Not equal: {age: {$ne: 25 }}
+      $or: or (either)
+        {$or: [{name: 'Alice'}, {name: 'Bob'}]}
+  */
   find: function(params, callback) {
     var self = this;
 
     self.ready.then(function(connection){
-      var query = params.query;
+      var query = buildQuery(params.query);
+
       // Start with finding all, and limit when necessary.
       var result = d.q(query, connection);
       callback(result);
@@ -111,4 +143,125 @@ export const Service = Proto.extend({
 
 export default function() {
   return Proto.create.apply(Service, arguments);
+}
+
+import filter from 'filter-object';
+
+// http://www.2ality.com/2015/11/stage3-object-entries.html
+function* entries(obj) {
+   for (let key of Object.keys(obj)) {
+     yield [key, obj[key]];
+   }
+}
+
+
+function buildQuery(params) {
+  let query = [];
+  let wheres = {};
+  let clauses = {};
+
+  // WIP: this is mainly a sketch of idea/algorithm
+  // UNTESTED... Needs more love/work!
+
+  params.$equals = filter(params, '!$*')
+
+  let selectSet = {
+    invalid: filterSelect(params.$select, -1),
+    valid: filterSelect(params.$select, 1),
+  };
+  var selects = Object.keys(params.$equals);
+
+  // for select/where, ideally we would use $in to pass values as arguments,
+  // however this may be too complicated for now. 
+  // Primitive string building of hard coded test values in :where 
+  // looks (MUCH) easier for a first POC (prototype!)
+
+  wheres.eq = buildWhereEqs(params.$equals);
+
+  // $ne: Not equal: {age: {$ne: 25 }}
+  where.not = buildNots(params.$ne)
+
+  // $in, $nin: Contains (or not) given value in list
+  //   {name: {$in: ['Alice', 'Bob']}}
+
+  // Translations:
+  // $or and $in are equivalent
+  // $nin is simply an $or nested with a list of $ne inside (I think?)
+
+  // $lt, $lte: less than <, <=
+  //   {age: {$lte: 65 }}
+
+  // $gt, $gte: less than >, >=
+  //   {age: {$gte: 21 }}
+
+
+  let predicates = {
+    '<': '$lt',
+    '<=': '$lte',
+    '>': '$gt',
+    '>=': '$gte'
+  };
+  let predKeys = Object.keys(predicates);
+  let predicateFuns = Object.keys(predicates).reduce((prev, next) =>{
+    return Object.assign(prev, {[next]: predicateBuilder(next)};)
+  }, {});
+
+  let wherePreds = predicateExprs(predicates, predicateFuns);
+
+  Object.assign(where, wherePreds);
+
+  // collect all the where object values into one list of where clauses
+  let clauses.where = [].concat(Object.values(where));
+
+  // More TODO...
+
+  // TODO: return query we have built
+  return params;
+}
+
+function predicateExprs(predicates, predicateFuns) {
+  var whereClauses = [];
+  for (let [k, v] of entries(predicates)) {
+    let predFun = predicateFuns[k];
+    whereClauses.push(predFun(k, v));
+  }
+  return whereClauses;
+}
+
+function predicateBuilder(predicate) {
+  return function predicateExpr(attribute, value) {
+    return `[(${predicate} ?${attribute} ${value})]]`;
+  }
+}
+
+function filterSelect(obj, criteria) {
+  return Object.keys(filter(obj, criteria));
+}
+
+// TODO: use a class for each type of clause!?
+
+function buildWhereEqs(list) {
+  var whereClauses = [];
+  for (let [k,v] of entries(obj)) {
+    whereClauses.push(createWhereClause(k, v));
+  }
+  return whereClauses;
+}
+
+function createWhereClause(k, v) {
+  return `[?eid ${attribute} ${value}]`;
+}
+
+function buildNots(obj) {
+  obj.map(not => {
+    var clauses = [];
+    for (let [k,v] of entries(obj)) {
+      clauses.push(createNotClause(k, v));
+    }
+    return clauses;
+  })
+}
+
+function createNotClause(attribute, value) {
+  return `(not [?eid ${attribute} ${value}])`
 }
