819c7080fc8b4007ea181e95bd479e456d1b556e
Kristian Mandrup <kmandrup@gmail.com>
2016-03-27 17:49:46 +0200

    test skeletons

---
+++ test/query/where/index.test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+
+let expect = chai.expect;
+
+describe('feathers-datascript', () => {
+  // before(clean);
+  // after(clean);
+
+  beforeEach(done => {
+    done();
+  });
+
+  afterEach(done => {
+    done();
+  });
+
+  it('basic functionality', done => {
+    expect(typeof 1).to.equal('number');
+    done();
+  });
+});
