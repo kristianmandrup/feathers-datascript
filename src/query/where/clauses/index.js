e9faef7c4427e676cc1d5aee8031c023b680d6c1
Kristian Mandrup <kmandrup@gmail.com>
2016-03-29 17:47:20 +0200

    extract query builder to external repo

--- src/query/where/clauses/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Id } from './id';
-export { default as Eq } from './eq';
-export { default as NotEq } from './not_eq';
-export { default as Or } from './or';
-export { default as Predicate } from './predicate';
